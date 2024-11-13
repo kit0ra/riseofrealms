75ebfe9794201c697f6fa7ec6c90867468b2cd49
kit0ra <noovaomega@gmail.com>
2024-11-13 13:18:47 +0100

    Initial project setup

---
+++ src/main/java/fr/univbordeaux/riseofrealms/App.java
@@ -0,0 +1,13 @@
+package fr.univbordeaux.riseofrealms;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
