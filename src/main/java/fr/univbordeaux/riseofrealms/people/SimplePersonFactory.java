9475af85a6fc8fd11933afb3fe976c9e7243c0f4
ANIS BETTAIEB <130476790+Aniis05@users.noreply.github.com>
2024-12-06 16:16:05 +0100

    Create SimplePersonFactory.java
    implementation  SimplePersonFactory

---
+++ src/main/java/fr/univbordeaux/riseofrealms/people/SimplePersonFactory.java
@@ -0,0 +1,12 @@
+package fr.univbordeaux.riseofrealms.person;
+
+public class SimplePersonFactory implements PersonFactory {
+    @Override
+    public Person createPerson(String type, String name, int age) {
+        return switch (type.toLowerCase()) {
+            case "worker" -> new Worker(name, age);
+            case "resident" -> new Resident(name, age);
+            default -> throw new IllegalArgumentException("Unknown person type: " + type);
+        };
+    }
+}
